a59eb9bfdd837d552e25fa125373cb7f7d923c83
yunon <38744862+yunonXD@users.noreply.github.com>
2021-09-07 18:32:21 +0900

    프로젝트 합침
    + 앵커 등록 안되어있어서 해상도 1920 1080으로 고정시킴

---
+++ Assets/Map_Icon_button.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Map_Icon_button : MonoBehaviour
+{
+    public GameObject panel;
+    // Start is called before the first frame update
+
+    public void Bigicon_Map()
+    {
+        panel.SetActive(true);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/ScreenRock.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ScreenRock : MonoBehaviour
+{
+
+        void Awake(){
+        Screen.SetResolution(1920, 1080, true);
+
+    }
+}
+
+
