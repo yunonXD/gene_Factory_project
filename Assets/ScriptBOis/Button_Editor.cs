908f375207de4febff0e9479c1462c9f1942e884
yunon <38744862+yunonXD@users.noreply.github.com>
2021-09-11 20:58:22 +0900

    프러젝트통합
    ESC일시정지-계속하기-타이틀로-게임종료

--- Assets/ScriptBOis/Button_Editor.cs
+++ Assets/ScriptBOis/Button_Editor.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class Button_Editor : MonoBehaviour{
 
+    public GameObject Optionpage;
+
     public void SceneChange_inGame(){
 
         SceneManager.LoadScene("inGameScene");
@@ -18,10 +20,28 @@ public class Button_Editor : MonoBehaviour{
         SceneManager.LoadScene("BattleSelectScene");
     }
 
+    public void SceneChange_Title()
+    {
+        SceneManager.LoadScene("IntroScene");
+    }
+
+    public void OptionSetActive()
+    {
+        Optionpage.SetActive(false);
+    }
 
     public void OnApplicationQuit(){
         Application.Quit();
         Debug.Log("ÇØÄ¡¿ü³ª?");
     }
 
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Optionpage.SetActive(true);
+        }
+    }
 }
